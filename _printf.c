6a8c1e13b7968fad20c97abc82b94978639bb333
jimahlove <jimahdasy034@gmail.com>
2023-04-25 15:19:14 -0700

    i will strike back

--- _printf.c
+++ _printf.c
@@ -1,7 +1,9 @@
-include "main.h"
+#include <stddef.h>
+#include "main.h"
+
 void print_buffer(char buffer[], int *buff_ind);
 /**
-* _printf - Printf function
+* _printf - all Printf function declare
 * @format: format.
 * Return: Printed chars.
 */
@@ -10,7 +12,7 @@ int _printf(const char *format, ...)
 int i, printed = 0, printed_chars = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
-char buffer[BUFF_SIZE];
+char buffer [BUFF_SIZE];
 if (format == NULL)
 return (-1);
 va_start(list, format);
@@ -18,7 +20,7 @@ for (i = 0; format && format[i] != '\0'; i++)
 {
 if (format[i] != '%')
 {
-buffer[buff_ind++] = format[i];
+buffer [buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
 print_buffer(buffer, &buff_ind);
 /* write(1, &format[i], 1);*/
@@ -44,6 +46,15 @@ va_end(list);
 return (printed_chars);
 }
 /**
-* print_buffer - Prints the contents of the buffer if it exist
-* @buffer: Array of chars
-*/
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
